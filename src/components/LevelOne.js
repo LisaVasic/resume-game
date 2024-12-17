65c27a8ba369c134044a33c46cc4ef51ebacc9a5
Lisa Johansson <Lisa@Lisas-MacBook-Pro.local>
2024-12-17 09:56:11 +0100

    Message correct/wrong when clicking submit

--- src/components/LevelOne.js
+++ src/components/LevelOne.js
@@ -1,42 +1,74 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const correctAnswerOne = 'number';
+const correctAnswerTwo = 'ageToCheck'
 
+// allows us to use and change the user input(value)
 export const LevelOne = () => {
-  const [inputValue, setInputValue] = useState(''); // allows us to use and change the user input(value)
-  const [submittedValue, setSubmittedValue] = useState('');
+  const [inputValue, setInputValue] = useState({
+    blankOne: '',
+    blankTwo: ''
+  });
+
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isfalse, setIsFalse] = useState(false);
+  // const [submittedValue, setSubmittedValue] = useState(false);
 
   // // Event handler for submit button, so the value changes when clicked
   const handleSubmit = () => {
-    setSubmittedValue(inputValue);
+    if (inputValue.blankOne === correctAnswerOne && inputValue.blankTwo === correctAnswerTwo) {
+      setIsCorrect(true);
+    } else {
+      setIsFalse(true);
+    }
   };
 
   // Event handler for input changes
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    const { name, value } = e.target;
+    setInputValue((prevState) => ({
+      ...prevState, // Copy previous state
+      [name]: value // Update the specific field
+    }));
   };
 
   // Check the submitted answer
-  if (submittedValue === correctAnswerOne) {
-    alert('Correct');
-  }
+  // if (inputValue.blankOne === correctAnswerOne
+  //     && inputValue.blankTwo === correctAnswerTwo) {
+  //   alert('Correct');
+  // }
 
   return (
-    <outerContainer>
-      <innerContainer>
+    <OuterContainer>
+      <InnerContainer>
         <h1>Explanations</h1>
-        <questionContainer>
+        <QuestionContainer>
           Fill in the blanks to complete the TypeScript code:
           <pre>
             {'let age: '}
-            <input type="text" value={inputValue} onChange={handleInputChange} /> {/* number */}
+            <input type="text" name="blankOne" value={inputValue.blankOne} onChange={handleInputChange} /> {/* number */}
             {' = 25;\n\nfunction isAdult('}
-            <input type="text" /> {/* ageToCheck :number */}
-            {'): boolean {\n  return age >= 18;\n}'}
+            <input type="text" name="blankTwo" value={inputValue.blankTwo} onChange={handleInputChange} /> {/* ageToCheck */}
+            {':number): boolean {\n  return age >= 18;\n}'}
           </pre>
+          {isCorrect && <p>Correct!</p>}
+          {isfalse && <p>Try again!</p>}
           <button type="submit" id="guessSubmit" onClick={handleSubmit}>Submit</button>
-        </questionContainer>
-      </innerContainer>
-    </outerContainer>
+        </QuestionContainer>
+      </InnerContainer>
+    </OuterContainer>
   );
 }
+
+export const OuterContainer = styled.div`
+
+`;
+
+export const InnerContainer = styled.div`
+`;
+
+export const QuestionContainer = styled.div`
+
+`;
+
