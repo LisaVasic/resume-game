d36d92ab1bbf0cd2be2155edf0ef58e368e7e14c
Lisa Johansson <Lisa@Lisas-MacBook-Pro.local>
2024-12-20 16:22:54 +0100

    level 3 game board and on click function

--- src/components/LevelThree.js
+++ src/components/LevelThree.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export const LevelThree = () => {
-  useState(true);
+  const correctPath = ['step1', 'step2', 'step5', 'step8', 'step9']
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const handleStep = (value) => {
+    if (value === correctPath[currentStep]) {
+      setCurrentStep(currentStep + 1); // Move to the next step
+      alert('yes');
+    } else {
+      setCurrentStep(0); // Reset if the user clicks the wrong step
+      alert('no')
+    }
+  };
+
   return (
     <OuterContainer>
       <InnerContainer>
         <h1>Rubrik</h1>
         <p>Förklaring</p>
         <GameBoard>
-          <Circle />
+          <Circle onClick={() => handleStep('step1')} />
+          <Circle onClick={() => handleStep('step2')} />
+          <Circle onClick={() => handleStep('step3')} />
+          <Circle onClick={() => handleStep('step4')} />
+          <Circle onClick={() => handleStep('step5')} />
+          <Circle onClick={() => handleStep('step6')} />
+          <Circle onClick={() => handleStep('step7')} />
+          <Circle onClick={() => handleStep('step8')} />
+          <Circle onClick={() => handleStep('step9')} />
         </GameBoard>
       </InnerContainer>
     </OuterContainer>
@@ -17,21 +37,33 @@ export const LevelThree = () => {
 }
 
 export const OuterContainer = styled.div`
+border: solid 2px blue;
+background-color: pink;
 
 `;
 
 export const InnerContainer = styled.div`
-align content: center;
+display:flex;
+flex-direction: column;
+justify-content: center;
+background-color: grey;
 `;
 
 export const GameBoard = styled.div`
-  align-content: center;
-  border: solid 2px black;
+width: 50%;
+background-color: beige;
   display: grid;
+  justify-content: center;
+  border: solid 2px black;
   grid-template-columns: repeat(3, 100px);
   grid-template-rows: repeat(3, 100px);
-  gap: 10px;
+  gap: 20px;
+  padding:20px;
 `;
 
 export const Circle = styled.div`
+border: solid 5px blue;
+border-radius: 50%;
+
+
 `;
