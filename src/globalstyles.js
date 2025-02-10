8578fc8008595586b022a2ef832631337b2950af
Lisa Johansson <109794046+LisaVasic@users.noreply.github.com>
2025-02-10 09:54:36 +0100

    Track globalstyles.js properly

---
+++ src/globalstyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+// Styling for mobile
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Pacifico&display=swap');
+
+  body {
+    font-family: "Nunito", serif;
+    font-optical-sizing: auto;
+    font-weight: 700;
+    font-style: normal;
+    color: #356D65;
+  }
+`;
+
